83eba3ba4dc508a90ab9e3685dd5d2bcab0d4d16
Patmish <patmish.kumar@gmail.com>
2025-04-08 15:29:59 +0800

    test

--- mystore/place/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import user
-
-class userForm(forms.ModelForm):
-    
-    class Meta:
-        model = user
-        fields = ["email", "username" , "passwd"]
