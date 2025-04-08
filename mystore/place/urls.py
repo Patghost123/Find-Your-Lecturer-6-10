168e00d1391a3feb6b5cd2a20f23b0b1335d7bf9
Aevum0001 <primegamezzz20@gmail.com>
2025-04-09 00:06:40 +0800

    clone 2

---
+++ mystore/place/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+from .views import join, login, hello
+
+urlpatterns = [
+    path('', views.hello, name="hello"),
+    path('join/', views.join, name="join"),
+    path('login/', views.login, name="login"),
+    path('hello/', views.hello, name="hello"),
+    path('', include('place.urls')),
+] 
