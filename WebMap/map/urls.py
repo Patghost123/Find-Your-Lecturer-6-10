8f5a554081f4c4fa3085852cf898881ce16e06e3
Patghost123 <Patmish.kumar@gmail.com>
2025-05-17 02:13:26 +0800

    added a new field called slug so able to search for the lecturers with the / in their name

--- WebMap/map/urls.py
+++ WebMap/map/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from map.views import students_list
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import get_lecturers, lecturer_profile
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -16,7 +14,7 @@ urlpatterns = [
     path("students/", views.students_list, name="students_list"),
     path('map/<int:floor_number>/', views.floor_map, name='floor_map'),
     path("get_lecturers/", views.get_lecturers, name="get_lecturers"),
-    path("lecturer/<str:lecturer_name>/", views.lecturer_profile, name="lecturer_profile"),
+    path("lecturer/<slug:slug>/", views.lecturer_profile, name="lecturer_profile"),
 ]
 
 if settings.DEBUG:
